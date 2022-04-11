679c8149c348dfed85b2b042e1802625e5093191
Quakua-sp <103194500+Quakua-sp@users.noreply.github.com>
2022-04-11 14:14:28 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-location-2",
+      "name": "Location 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -1.281990726007752,
+        "pitch": -0.04497807389506292,
+        "fov": 1.0044703620608095
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
