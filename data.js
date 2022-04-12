9c406ce91b10b871c0caa44943161054b0b0850d
Quakua-sp <103194500+Quakua-sp@users.noreply.github.com>
2022-04-12 10:36:34 +0100

    Update data.js

--- data.js
+++ data.js
@@ -41,21 +41,34 @@ var APP_DATA = {
         {
           "yaw": 3.124769549624073,
           "pitch": 0.08707013738223601,
-          "popup": "uob-lapworth-collections-08nov21-2.jpg",
           "title": "Precambrian life - Charnia",
-          "text": "Charnia Bottom text"
+          "text": "Charnia Bottom text",
+          "popup": "uob-lapworth-collections-08nov21-2.jpg"
+        },
+        {
+          "yaw": 3.124769549624073,
+          "pitch": 0.15612590442288266,
+          "title": "Precambrian life - Charnia",
+          "text": "This cast of a rock surface from Charnwood Forest near Leicester, has impressions of Charnia masoni.This is a very important fossil discovery because it proved for the first time the existence of Precambrian life. It was found in 1957 by a 15-year-old Roger Mason who first reported it. Another 15 year old,Tina Negus, had actually recognised the 560million-year-old fossil a year earlier, but her schoolteachers dismissed her discovery! Charnia are frond-like organisms attached to the seafloor, but it is not known exactly how they lived.  Various fossils of this age have since been found worldwide and are known as Ediacaran biota."
         },
         {
           "yaw": -1.262363825387638,
           "pitch": 0.15612590442288266,
           "title": "Allosaurus 'Roary'",
-          "text": "Allosaurus bottom text"
+          "text": "This is Roary, a young Allosaurus which lived 155 million years ago during the Jurassic period.This theropod dinosaur was the apex predator of its environment.Despite being a ferocious hunter, Roary was still susceptible to injury. The most noticeable of these can be seen as a large abnormal bone growth on the right foot. This was likely caused by a break which later became badly infected. With a damaged foot, Roary would no longer have hunted as successfully, possibly explaining why it did not reach adulthood."
+        },
+                {
+          "yaw": 2.462642801369813,
+          "pitch": 0.17670500270105762,
+          "title": "Geological Timeline",
+          "text": "Image of geological timeline",
+          "popup": "uob-lapworth-collections-08nov21-30.jpg"
         },
         {
           "yaw": 2.462642801369813,
           "pitch": 0.07670500270105762,
-          "title": "Evolution of Life",
-          "text": "EoL bottom text"
+          "title": "Geological Timeline",
+          "text": "The geological timeline shows how Earth history is split into many time units. They vary in length with some being long and other quite short. Boundaries between them are usually marked by major changes in environments and ecosystems such as the extinction of dinosaurs at the Cretaceous-Palaeogene boundary. Take a journey back through time, starting with the oldest time unit, the Precambrian, and ending with the most recent, the Quaternary, to discover how life has changed and evolved."
         }
       ]
     }
